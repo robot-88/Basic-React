baa36248b5c13fe31a269acaa0c18958235f17df
bot-88 <157427868+robot-88@users.noreply.github.com>
2024-05-21 09:54:45 +0000

    Commented  code

--- src/components/Login/LoginForm.js
+++ src/components/Login/LoginForm.js
@@ -5,27 +5,28 @@ import { FaLock } from "react-icons/fa";
 
 const LoginForm = () => {
   return (
-    <div className='wrapper'>
-      <form action="">
-        <h1>Login</h1>
-        <div className='input-box'>
-          <input type='text' placeholder='Username'/>
-          {/* <FaUser className='icon' /> */}
-        </div>
-        <div className='input-box'>
-          <input type='password' placeholder='Password' />
-          {/* <FaLock className='icon' /> */}
-        </div>
-        <div className='input-remember'>
-         <label> <input type='checkBox'/> Remember me</label> 
-         <a href='#'>Forgot Password</a>
-        </div>
-        <button type='submit'>Login</button>
-        <div className='register-link'>
-          <p>Don't have an account?<a href="#">Register</a></p>
-        </div>
-      </form>
-    </div>
+    // <div className='wrapper'>
+    //   <form action="">
+    //     <h1>Login</h1>
+    //     <div className='input-box'>
+    //       <input type='text' placeholder='Username'/>
+    //       {/* <FaUser className='icon' /> */}
+    //     </div>
+    //     <div className='input-box'>
+    //       <input type='password' placeholder='Password' />
+    //       {/* <FaLock className='icon' /> */}
+    //     </div>
+    //     <div className='input-remember'>
+    //      <label> <input type='checkBox'/> Remember me</label> 
+    //      <a href='#'>Forgot Password</a>
+    //     </div>
+    //     <button type='submit'>Login</button>
+    //     <div className='register-link'>
+    //       <p>Don't have an account?<a href="#">Register</a></p>
+    //     </div>
+    //   </form>
+    // </div>
+    <div>Login Form</div>
   )
 }
 
